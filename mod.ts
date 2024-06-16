dd31868a57cbd1479cefdf71d67b3649e206a0d9
Filip Jirsák <filip@jirsak.org>
2024-06-16 02:40:08 +0200

    Publish type RESTfulGlobalOptions.

--- mod.ts
+++ mod.ts
@@ -4,6 +4,7 @@ export {
   type KeyBuilders,
   type RESTfulCollection,
   type RESTfulCollections,
+  type RESTfulGlobalOptions,
   type RESTfulOptions,
   type RESTfulOptionsAll,
   type ResultWithMetadata,
